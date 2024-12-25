d14698a6b538094acde19883232192f37f96ead2
Jadin Heaston <86203688+JadinHeaston@users.noreply.github.com>
2024-12-24 19:12:54 -0600

    Fixing nullable type.
    Signed-off-by: Jadin Heaston <86203688+JadinHeaston@users.noreply.github.com>

--- includes/models.php
+++ includes/models.php
@@ -270,48 +270,6 @@ class TrackingDB extends DatabaseConnector
 	}
 }
 
-class Mailer
-{
-	public $senderEmail;
-
-	private function __construct(string $senderEmail)
-	{
-		$this->senderEmail = $senderEmail;
-	}
-
-	public function sendMail(array|string $destination, string $subject, string $message, array|string $carbonCopy = '', array|string $blindCarbonCopy = '', array $additionalHeaders = array())
-	{
-		//Formatting destination.
-		if (is_array($destination))
-			$destination = implode(',', $destination);
-		if (is_array($carbonCopy))
-			$carbonCopy = implode(',', $carbonCopy);
-		if (is_array($blindCarbonCopy))
-			$blindCarbonCopy = implode(',', $blindCarbonCopy);
-
-
-		$headers['From'] = $this->senderEmail;
-
-		if ($carbonCopy !== '')
-			$headers['CC'] = $carbonCopy;
-		if ($blindCarbonCopy !== '')
-			$headers['BCC'] = $blindCarbonCopy;
-
-		$headers['MIME-Version'] = '1.0';
-		$headers['Content-type'] = 'text/html';
-
-		foreach ($additionalHeaders as $name => $header)
-		{
-			$headers[$name] = $header;
-		}
-		mail($destination, $subject, $message, $headers);
-	}
-
-	// private function checkEmailSentStatus()
-	// {
-	// }
-}
-
 class DBBConnector extends DatabaseConnector
 {
 	public function dumpDatabase(string $databaseName, array $excludedTables = []): bool
@@ -404,7 +362,7 @@ class DatabaseConfig
 		DatabaseType $databaseType,
 		bool $active = true,
 		bool $visitbility = true,
-		int $maxBackupCount = 14,
+		?int $maxBackupCount = 14,
 		array $excludedTables = []
 	)
 	{
